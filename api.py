f3bf000ba0042d0d6e2bc01c193a6b559a85d90a
Austin Dodd <adubdodd@gmail.com>
2020-10-26 00:47:29 -0400

    added requirements and test files for docker test_env

---
+++ api.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from test import get_odd_json
+
+app = Flask(__name__)
+api = Api(app)
+
+class Team(Resource):
+    def get(self):
+        return get_odd_json()[0]
+
+api.add_resource(Team, '/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ test.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
+def draftkings_parse(input_dict, driver, key):
+    outcomes = [x.text for x in driver.find_elements_by_class_name(input_dict['outcomes_class'])]
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    odds = [x.text for x in soup.findAll('span',{'class', input_dict['odds_class']})]
+    # odds = [x.text for x in driver.find_elements_by_class_name(input_dict['odds_class'])]
+    return list(zip(outcomes[::3], outcomes[2::3], odds[::3], odds[1::3], odds[2::3], [key] * len(outcomes[::3]), [datetime.now()] * len(outcomes[::3])))
+
+prem_dir = {'draftkings':{'url':'https://sportsbook.draftkings.com/leagues/soccer/53591936',
+                          'parser': draftkings_parse,
+                          'args':{'outcomes_class':'sportsbook-outcome-cell__label',
+                                  'odds_class':'sportsbook-odds american default-color'}}
+           }
+           
+output_cols = ['home_team','away_team', 'home_odds','tie_odds', 'away_odds', 'source', 'datetime']
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--window-size=1920,1200')
+DRIVER_PATH = 'driver/mac/chromedriver'
+
+def get_odd_json():
+    dn = []
+    for key in prem_dir:
+        driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+        driver.get(prem_dir[key]['url'])
+        driver.implicitly_wait(3)
+
+        data = pd.DataFrame(prem_dir[key]['parser'](prem_dir[key]['args'],driver, key),columns=output_cols)
+        dn.append(data)
+        driver.quit()
+    return pd.concat(dn, axis=0)[['home_team']].head().to_dict(orient='records')
+
